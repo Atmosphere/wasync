77f78cb099cae17c121c8e8cb80562a259bfab7f
jfarcand <jfarcand@apache.org>
2013-04-04 10:13:22 -0400

    Add Node.js sample

---
+++ samples/node.js/README.md
@@ -0,0 +1,11 @@
+### Using the sample
+
+Build the sample and then install WebSocket support in node.js
+
+```bash
+% cd src/main/node.js
+% npm install node-static
+% npm install websocket
+% node chat-server.js
+```
+Then point the browser to 127.0.0.1:8080
---
+++ samples/node.js/src/main/java/org/atmosphere/wasync/samples/wAsyncNodeChat.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2013 Jeanfrancois Arcand
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.atmosphere.wasync.samples;
+
+import org.atmosphere.wasync.Client;
+import org.atmosphere.wasync.ClientFactory;
+import org.atmosphere.wasync.Decoder;
+import org.atmosphere.wasync.Encoder;
+import org.atmosphere.wasync.Function;
+import org.atmosphere.wasync.Options;
+import org.atmosphere.wasync.Request;
+import org.atmosphere.wasync.RequestBuilder;
+import org.atmosphere.wasync.Socket;
+import org.atmosphere.wasync.Transport;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Date;
+
+public class wAsyncNodeChat {
+
+    private final static Logger logger = LoggerFactory.getLogger(wAsyncNodeChat.class);
+    private final static ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) throws IOException {
+
+        if (args.length == 0) {
+            args = new String[] {"http://127.0.0.1:8080"};
+        }
+
+        Options options = new Options.OptionsBuilder().build();
+        Client client = ClientFactory.getDefault().newClient();
+
+        RequestBuilder request = client.newRequestBuilder()
+                .method(Request.METHOD.GET)
+                .uri(args[0] + "/chat")
+                .encoder(new Encoder<String, Data>() {
+                    @Override
+                    public Data encode(String s) {
+                        try {
+                            return mapper.readValue(s, Data.class);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                })
+                .encoder(new Encoder<Data, String>() {
+                    @Override
+                    public String encode(Data data) {
+                        try {
+                            return mapper.writeValueAsString(data);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                })
+                .decoder(new Decoder<String, Data>() {
+                    @Override
+                    public Data decode(Transport.EVENT_TYPE type, String data) {
+
+                        data = data.trim();
+
+                        // Padding
+                        if (data.length() == 0) {
+                            return null;
+                        }
+
+                        if (type.equals(Transport.EVENT_TYPE.MESSAGE)) {
+                            try {
+                                return mapper.readValue(data, Data.class);
+                            } catch (IOException e) {
+                                logger.debug("Invalid message {}", data);
+                                return null;
+                            }
+                        } else {
+                            return null;
+                        }
+                    }
+                })
+                .transport(Request.TRANSPORT.WEBSOCKET)
+                .transport(Request.TRANSPORT.LONG_POLLING);
+
+        Socket socket = client.create(options);
+        socket.on("message", new Function<Data>() {
+            @Override
+            public void on(Data t) {
+                Date d = new Date(t.getTime()) ;
+                logger.info("Author {}: {}", t.getAuthor() + "@ " + d.getHours() + ":" + d.getMinutes(), t.getMessage());
+            }
+        }).on(new Function<Throwable>() {
+
+            @Override
+            public void on(Throwable t) {
+                t.printStackTrace();
+            }
+
+        }).open(request.build());
+
+        logger.info("Choose Name: ");
+        String name = null;
+        String a = "";
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (!(a.equals("quit"))) {
+            a = br.readLine();
+            if (name == null) {
+                name = a;
+            }
+            socket.fire(new Data(name, a));
+        }
+        socket.close();
+    }
+
+
+}
+
