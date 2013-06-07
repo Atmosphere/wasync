a90c639bd34d5e616f1db9a4fb48e18649d0bbd3
jfarcand <jfarcand@apache.org>
2013-06-07 15:57:26 -0400

    Reformat

--- wasync/src/test/java/org/atmosphere/tests/LongPollingTest.java
+++ wasync/src/test/java/org/atmosphere/tests/LongPollingTest.java
@@ -43,174 +43,174 @@ public class LongPollingTest extends StreamingTest {
     int notFoundCode() {
         return 404;
     }
-    
+
     @Test
     public void BinaryEchoTest() throws Exception {
-    	Config config = new Config.Builder()
-    		.port(port)
-    		.host("127.0.0.1")    	
-    		.resource("/suspend", new AtmosphereHandler() {
+        Config config = new Config.Builder()
+                .port(port)
+                .host("127.0.0.1")
+                .resource("/suspend", new AtmosphereHandler() {
 
-        	@Override
-        	public void onRequest(AtmosphereResource resource) throws IOException {
-        		if (resource.getRequest().getMethod().equals("GET")) {
-        			resource.suspend(-1);
-        		} else {
-        			int payloadSize = resource.getRequest().getContentLength();
-					byte[] payload = new byte[payloadSize];
-					try {
-						resource.getRequest().getInputStream().read(payload);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-        			logger.info("echoing : {}", payload);
-        			resource.getBroadcaster().broadcast(payload);
-        		}
-        	}
+                    @Override
+                    public void onRequest(AtmosphereResource resource) throws IOException {
+                        if (resource.getRequest().getMethod().equals("GET")) {
+                            resource.suspend(-1);
+                        } else {
+                            int payloadSize = resource.getRequest().getContentLength();
+                            byte[] payload = new byte[payloadSize];
+                            try {
+                                resource.getRequest().getInputStream().read(payload);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                            logger.info("echoing : {}", payload);
+                            resource.getBroadcaster().broadcast(payload);
+                        }
+                    }
 
-        	@Override
-        	public void onStateChange(AtmosphereResourceEvent event) throws IOException {
-        		if (!(event.isResuming()) || event.isResumedOnTimeout() || event.isSuspended()) {
-    				// make the GET reply have binary content type
-    				event.getResource().getResponse().setContentType("application/octet-stream");
-    				// make it use the OutputStream directly in writing, prevent any String conversions.
-    				event.getResource().getRequest().setAttribute(ApplicationConfig.PROPERTY_USE_STREAM, true);
-    				// do the actual write
-    				event.getResource().getResponse().write((byte[])event.getMessage());        
-        			event.getResource().resume();
-        		}
-        	}
-        	
-        	@Override
-        	public void destroy() {
-        		
-        	}
-        	
-        }).build();
+                    @Override
+                    public void onStateChange(AtmosphereResourceEvent event) throws IOException {
+                        if (!(event.isResuming()) || event.isResumedOnTimeout() || event.isSuspended()) {
+                            // make the GET reply have binary content type
+                            event.getResource().getResponse().setContentType("application/octet-stream");
+                            // make it use the OutputStream directly in writing, prevent any String conversions.
+                            event.getResource().getRequest().setAttribute(ApplicationConfig.PROPERTY_USE_STREAM, true);
+                            // do the actual write
+                            event.getResource().getResponse().write((byte[]) event.getMessage());
+                            event.getResource().resume();
+                        }
+                    }
 
-    	Nettosphere server = new Nettosphere.Builder().config(config).build();
-    	assertNotNull(server);
-    	server.start();
-   
-    	final CountDownLatch latch = new CountDownLatch(1);
-    	final AtomicBoolean hasEchoReplied = new AtomicBoolean(false);
-    	AtmosphereClient client = ClientFactory.getDefault().newClient(AtmosphereClient.class);
-    	
-    	final byte[] binaryEcho = new byte[] {1,2,3,4};
+                    @Override
+                    public void destroy() {
 
-    	RequestBuilder request = client.newRequestBuilder()
-		   .method(Request.METHOD.GET)
-		   .uri(targetUrl + "/suspend")
-		   .header("Content-Type", "application/octet-stream")
-		   .transport(Request.TRANSPORT.LONG_POLLING);
+                    }
 
-    	Socket socket = client.create(client.newOptionsBuilder().build());
+                }).build();
 
-    	socket.on("message", new Function<byte[]>() {    		
-    		@Override
-    		public void on(byte[] message) {
-    			logger.info("received : {}", message);
-    			if (Arrays.equals(message,binaryEcho)) {
-    				hasEchoReplied.getAndSet(true);
-    			}
-    			latch.countDown();
-    		}
-    	}).on(new Function<Throwable>() {
-    		@Override
-    		public void on(Throwable t) {
-    			t.printStackTrace();
-    		}
-    	}).open(request.build());
+        Nettosphere server = new Nettosphere.Builder().config(config).build();
+        assertNotNull(server);
+        server.start();
 
-    	socket.fire(binaryEcho).get();
-    	
-    	latch.await(2, TimeUnit.SECONDS);
-    	socket.close();
+        final CountDownLatch latch = new CountDownLatch(1);
+        final AtomicBoolean hasEchoReplied = new AtomicBoolean(false);
+        AtmosphereClient client = ClientFactory.getDefault().newClient(AtmosphereClient.class);
 
-    	assertEquals(hasEchoReplied.get(), true);
+        final byte[] binaryEcho = new byte[]{1, 2, 3, 4};
+
+        RequestBuilder request = client.newRequestBuilder()
+                .method(Request.METHOD.GET)
+                .uri(targetUrl + "/suspend")
+                .header("Content-Type", "application/octet-stream")
+                .transport(Request.TRANSPORT.LONG_POLLING);
+
+        Socket socket = client.create(client.newOptionsBuilder().build());
+
+        socket.on("message", new Function<byte[]>() {
+            @Override
+            public void on(byte[] message) {
+                logger.info("received : {}", message);
+                if (Arrays.equals(message, binaryEcho)) {
+                    hasEchoReplied.getAndSet(true);
+                }
+                latch.countDown();
+            }
+        }).on(new Function<Throwable>() {
+            @Override
+            public void on(Throwable t) {
+                t.printStackTrace();
+            }
+        }).open(request.build());
+
+        socket.fire(binaryEcho).get();
+
+        latch.await(10, TimeUnit.SECONDS);
+        socket.close();
+
+        assertEquals(hasEchoReplied.get(), true);
     }
-    
+
     @Test
     public void noMessageLostTest() throws Exception {
-    	Config config = new Config.Builder()
-        	.port(port)
-        	.host("127.0.0.1")
-            .broadcasterCache(org.atmosphere.cache.UUIDBroadcasterCache.class)
-            .resource("/suspend", new AtmosphereHandler() {
+        Config config = new Config.Builder()
+                .port(port)
+                .host("127.0.0.1")
+                .broadcasterCache(org.atmosphere.cache.UUIDBroadcasterCache.class)
+                .resource("/suspend", new AtmosphereHandler() {
 
-            	@Override
-            	public void onRequest(AtmosphereResource resource) throws IOException {
-            		if (resource.getRequest().getMethod().equals("GET")) {
-            			resource.suspend(-1);
-            		} else {
-            			String echo = resource.getRequest().getReader().readLine();
-            			logger.info("echoing : {}", echo);
-            			resource.getBroadcaster().broadcast(echo);
-            		}
-            	}
-
-            	@Override
-            	public void onStateChange(AtmosphereResourceEvent event) throws IOException {
-                    if (List.class.isAssignableFrom(event.getMessage().getClass())) {
-                        List<String> cached = (List<String>) List.class.cast(event.getMessage());
-                        logger.info("cached : {}", cached);
-                        for (String m: cached) {
-                            event.getResource().getResponse().write(m);
+                    @Override
+                    public void onRequest(AtmosphereResource resource) throws IOException {
+                        if (resource.getRequest().getMethod().equals("GET")) {
+                            resource.suspend(-1);
+                        } else {
+                            String echo = resource.getRequest().getReader().readLine();
+                            logger.info("echoing : {}", echo);
+                            resource.getBroadcaster().broadcast(echo);
                         }
-                    } else {
-                        event.getResource().getResponse().write((String) event.getMessage());
                     }
-                    event.getResource().resume();
-            	}
-            	
-            	@Override
-            	public void destroy() {
-            		
-            	}
-            	
-            }).build();
 
-       Nettosphere server = new Nettosphere.Builder().config(config).build();
-       assertNotNull(server);
-       server.start();
-       
-       final CountDownLatch latch = new CountDownLatch(5);
-       final AtomicReference<Set> response = new AtomicReference<Set>(new HashSet());
-       AtmosphereClient client = ClientFactory.getDefault().newClient(AtmosphereClient.class);
+                    @Override
+                    public void onStateChange(AtmosphereResourceEvent event) throws IOException {
+                        if (List.class.isAssignableFrom(event.getMessage().getClass())) {
+                            List<String> cached = (List<String>) List.class.cast(event.getMessage());
+                            logger.info("cached : {}", cached);
+                            for (String m : cached) {
+                                event.getResource().getResponse().write(m);
+                            }
+                        } else {
+                            event.getResource().getResponse().write((String) event.getMessage());
+                        }
+                        event.getResource().resume();
+                    }
 
-       RequestBuilder request = client.newRequestBuilder()
-    		   .method(Request.METHOD.GET)
-    		   .uri(targetUrl + "/suspend")
-    		   .transport(Request.TRANSPORT.LONG_POLLING);
+                    @Override
+                    public void destroy() {
 
-       Socket socket = client.create(client.newOptionsBuilder().build());
+                    }
 
-       socket.on("message", new Function<String>() {
-    	   @Override
-    	   public void on(String message) {
-    		   logger.info("received : {}", message);
-    		   response.get().add(message);
-    		   latch.countDown();
-    	   }
-       }).on(new Function<Throwable>() {
-    	   @Override
-    	   public void on(Throwable t) {
-    		   t.printStackTrace();
-    	   }
-       }).open(request.build());
+                }).build();
 
-       socket.fire("ECHO1");
-       Thread.sleep(1000);
-       socket.fire("ECHO2");
-       Thread.sleep(2000);
-       socket.fire("ECHO3");
-       socket.fire("ECHO4");
-       socket.fire("ECHO5");
-       
-       latch.await(60, TimeUnit.SECONDS);
-       socket.close();
+        Nettosphere server = new Nettosphere.Builder().config(config).build();
+        assertNotNull(server);
+        server.start();
 
-       assertEquals(response.get().size(), 5);
-       
+        final CountDownLatch latch = new CountDownLatch(5);
+        final AtomicReference<Set> response = new AtomicReference<Set>(new HashSet());
+        AtmosphereClient client = ClientFactory.getDefault().newClient(AtmosphereClient.class);
+
+        RequestBuilder request = client.newRequestBuilder()
+                .method(Request.METHOD.GET)
+                .uri(targetUrl + "/suspend")
+                .transport(Request.TRANSPORT.LONG_POLLING);
+
+        Socket socket = client.create(client.newOptionsBuilder().build());
+
+        socket.on("message", new Function<String>() {
+            @Override
+            public void on(String message) {
+                logger.info("received : {}", message);
+                response.get().add(message);
+                latch.countDown();
+            }
+        }).on(new Function<Throwable>() {
+            @Override
+            public void on(Throwable t) {
+                t.printStackTrace();
+            }
+        }).open(request.build());
+
+        socket.fire("ECHO1");
+        Thread.sleep(1000);
+        socket.fire("ECHO2");
+        Thread.sleep(2000);
+        socket.fire("ECHO3");
+        socket.fire("ECHO4");
+        socket.fire("ECHO5");
+
+        latch.await(60, TimeUnit.SECONDS);
+        socket.close();
+
+        assertEquals(response.get().size(), 5);
+
     }
 }
