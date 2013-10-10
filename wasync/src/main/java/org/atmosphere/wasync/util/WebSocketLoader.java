f78f8d6723dfa085ddd3b0cebec8b9338de60299
jfarcand <jfarcand@apache.org>
2013-10-10 14:13:15 -0400

    Cosmetic

--- wasync/src/main/java/org/atmosphere/wasync/util/WebSocketLoader.java
+++ wasync/src/main/java/org/atmosphere/wasync/util/WebSocketLoader.java
@@ -16,6 +16,7 @@
 package org.atmosphere.wasync.util;
 
 import com.ning.http.client.AsyncHttpClient;
+import com.ning.http.client.AsyncHttpClientConfig;
 import org.atmosphere.wasync.Client;
 import org.atmosphere.wasync.ClientFactory;
 import org.atmosphere.wasync.Function;
@@ -29,6 +30,7 @@ import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * A utility class that can be used to load a WebSocket enabled Server
@@ -42,17 +44,20 @@ public class WebSocketLoader {
     public static void main(String[] s) throws InterruptedException, IOException {
 
         if (s.length == 0) {
-            s = new String[]{"500","100000","http://127.0.0.1:8080/simple/test"};
+            s = new String[]{"1", "10000", "http://10.0.1.4:8080/default/test"};
         }
 
         final int clientNum = Integer.valueOf(s[0]);
         final int messageNum = Integer.valueOf(s[1]);
         String url = s[2];
 
+        System.out.println("Connecting to: " + s[2]);
         System.out.println("Number of Client: " + clientNum);
         System.out.println("Number of Message: " + messageNum);
 
-        final AsyncHttpClient c = new AsyncHttpClient();
+        AsyncHttpClientConfig.Builder b = new AsyncHttpClientConfig.Builder();
+        b.setFollowRedirects(true).setIdleConnectionTimeoutInMs(-1).setRequestTimeoutInMs(-1).setConnectionTimeoutInMs(600000);
+        final AsyncHttpClient c = new AsyncHttpClient(b.build());
 
         final CountDownLatch l = new CountDownLatch(clientNum);
 
@@ -65,15 +70,20 @@ public class WebSocketLoader {
 
         long clientCount = l.getCount();
         final AtomicLong total = new AtomicLong(0);
+        final AtomicLong completed = new AtomicLong(0);
+        final AtomicInteger cc = new AtomicInteger();
 
         Socket[] sockets = new Socket[clientNum];
         for (int i = 0; i < clientCount; i++) {
             final AtomicLong start = new AtomicLong(0);
             sockets[i] = client.create(client.newOptionsBuilder().runtime(c).reconnect(false).build())
                     .on(new Function<Integer>() {
+
+
                         @Override
                         public void on(Integer statusCode) {
                             start.set(System.currentTimeMillis());
+                            System.out.println("Connected clients: " + cc.getAndIncrement());
                             l.countDown();
                         }
                     }).on(new Function<String>() {
@@ -86,9 +96,8 @@ public class WebSocketLoader {
                                 String[] m = s.split("\n\r");
                                 mCount += m.length;
                                 messages.countDown();
-                                System.out.println("Message left receive " + messages.getCount() + " message " + s);
                                 if (mCount == messageNum) {
-                                   // System.out.println("All messages received " + mCount);
+                                    System.out.println("All messages received " + mCount + " Completed: " + completed.incrementAndGet());
                                     total.addAndGet(System.currentTimeMillis() - start.get());
                                 }
                             }
@@ -112,16 +121,16 @@ public class WebSocketLoader {
 
         Socket socket = client.create(client.newOptionsBuilder().runtime(c).build());
         socket.open(request.build());
-        for (int i = 0; i < messageNum; i++ ) {
+        for (int i = 0; i < messageNum; i++) {
             socket.fire("message" + i);
         }
-        messages.await(5, TimeUnit.MINUTES);
+        messages.await();
         socket.close();
         for (int i = 0; i < clientCount; i++) {
             sockets[i].close();
         }
         c.close();
-        System.out.println("Total: " + (total.get()/clientCount));
+        System.out.println("Total: " + (total.get() / clientCount));
 
     }
 
