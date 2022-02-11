01989a9eb34e55970e9b91e6ce7f470127631c6e
jfarcand <jfarcand@apache.org>
2022-02-10 19:25:28 -0500

    Fix test

--- wasync/src/test/java/org/atmosphere/tests/BaseTest.java
+++ wasync/src/test/java/org/atmosphere/tests/BaseTest.java
@@ -15,26 +15,7 @@
  */
 package org.atmosphere.tests;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertNotNull;
-import static org.testng.Assert.assertNotSame;
-import static org.testng.Assert.assertTrue;
-
-import java.io.IOException;
-import java.io.Reader;
-import java.io.StringReader;
-import java.net.ConnectException;
-import java.net.ServerSocket;
-import java.util.Map;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicReference;
-
 import org.asynchttpclient.AsyncHttpClient;
-import org.asynchttpclient.AsyncHttpClientConfig;
 import org.asynchttpclient.DefaultAsyncHttpClient;
 import org.asynchttpclient.DefaultAsyncHttpClientConfig;
 import org.atmosphere.client.TrackMessageSizeInterceptor;
@@ -60,6 +41,24 @@ import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.net.ConnectException;
+import java.net.ServerSocket;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertNotSame;
+import static org.testng.Assert.assertTrue;
 
 public abstract class BaseTest {
     public final static String RESUME = "Resume";
@@ -312,7 +311,7 @@ public abstract class BaseTest {
 
         assertEquals(response.get(), RESUME);
     }
-    
+
     @Test
     public void basicWebSocketWithSizeTrackingOnTest() throws Exception {
         final CountDownLatch l = new CountDownLatch(1);
@@ -523,36 +522,27 @@ public abstract class BaseTest {
         assertEquals(map.get().getClass().getInterfaces()[0], Map.class);
 
     }
-    
+
     @Test
     public void status404FunctionTest() throws Exception {
         Config config = new Config.Builder()
                 .port(port)
                 .host("127.0.0.1")
-                .resource("/suspend", new AtmosphereHandler() {
+                .resource("/404", new AtmosphereHandler() {
 
                     private final AtomicBoolean b = new AtomicBoolean(false);
 
                     @Override
                     public void onRequest(AtmosphereResource r) throws IOException {
-                        if (!b.getAndSet(true)) {
-                            r.suspend(-1);
-                        } else {
-                            r.getBroadcaster().broadcast(RESUME);
-                        }
+                        r.getResponse().setStatus(404);
                     }
 
                     @Override
                     public void onStateChange(AtmosphereResourceEvent r) throws IOException {
-                        if (!r.isResuming() || !r.isCancelled()) {
-                            r.getResource().getResponse().getWriter().print(r.getMessage());
-                            r.getResource().resume();
-                        }
                     }
 
                     @Override
                     public void destroy() {
-
                     }
                 }).build();
 
@@ -567,7 +557,7 @@ public abstract class BaseTest {
 
         RequestBuilder request = client.newRequestBuilder()
                 .method(Request.METHOD.GET)
-                .uri(targetUrl + "/ratata")
+                .uri(targetUrl + "/404")
                 .transport(transport());
 
         final Socket socket = client.create(client.newOptionsBuilder().runtime(ahc, false).build());
@@ -667,19 +657,16 @@ public abstract class BaseTest {
 
         Socket socket = client.create(client.newOptionsBuilder().runtime(ahc, false).build());
 
-        IOException ioException = null;
-    
+        socket.on(new Function<ConnectException>() {
 
-            socket.on(new Function<ConnectException>() {
+            @Override
+            public void on(ConnectException t) {
+                response.set(t);
+                latch.countDown();
+            }
 
-                @Override
-                public void on(ConnectException t) {
-                    response.set(t);
-                    latch.countDown();
-                }
+        }).open(request.build());
 
-            }).open(request.build());
-     
 
         latch.await(5, TimeUnit.SECONDS);
         socket.close();
@@ -699,15 +686,15 @@ public abstract class BaseTest {
 
         Socket socket = client.create(client.newOptionsBuilder().runtime(ahc, false).build());
 
-            socket.on(new Function<IOException>() {
+        socket.on(new Function<IOException>() {
 
-                @Override
-                public void on(IOException t) {
-                    response.set(t);
-                    latch.countDown();
-                }
+            @Override
+            public void on(IOException t) {
+                response.set(t);
+                latch.countDown();
+            }
 
-            }).open(request.build());
+        }).open(request.build());
 
 
         latch.await(5, TimeUnit.SECONDS);
@@ -1421,24 +1408,24 @@ public abstract class BaseTest {
 
         final Socket socket = client.create(client.newOptionsBuilder().runtime(ahc, false).build());
 
-            socket.on(new Function<ConnectException>() {
+        socket.on(new Function<ConnectException>() {
 
-                @Override
-                public void on(ConnectException t) {
-                    socket.close(); // I close it here, remove this close, issue will not happen
-                    response.set(t);
-                    latch.countDown();
-                }
+            @Override
+            public void on(ConnectException t) {
+                socket.close(); // I close it here, remove this close, issue will not happen
+                response.set(t);
+                latch.countDown();
+            }
 
-            }).on(new Function<IOException>() {
+        }).on(new Function<IOException>() {
 
-                @Override
-                public void on(IOException t) {
-                    response2.set(t);
-                    latch.countDown();
-                }
+            @Override
+            public void on(IOException t) {
+                response2.set(t);
+                latch.countDown();
+            }
 
-            }).open(request.build());
+        }).open(request.build());
 
         socket.fire("echo");
         latch.await(5, TimeUnit.SECONDS);
@@ -1951,19 +1938,19 @@ public abstract class BaseTest {
 
         Socket socket = client.create(client.newOptionsBuilder().runtime(ahc, false).build());
 
-            socket.on(new Function<ConnectException>() {
+        socket.on(new Function<ConnectException>() {
 
-                @Override
-                public void on(ConnectException t) {
-                    latch.countDown();
-                }
+            @Override
+            public void on(ConnectException t) {
+                latch.countDown();
+            }
 
-            }).on(Event.CLOSE.name(), new Function<String>() {
-                @Override
-                public void on(String t) {
-                    logger.info("Connection closed");
-                }
-            }).open(request.build());
+        }).on(Event.CLOSE.name(), new Function<String>() {
+            @Override
+            public void on(String t) {
+                logger.info("Connection closed");
+            }
+        }).open(request.build());
 
 
         assertTrue(latch.await(10, TimeUnit.SECONDS));
