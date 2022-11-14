34c8fe8b09be4851efd1a353f96de06250ad8361
mokri <morten.hogsberg.futtrup.kristensen@systematic.com>
2022-11-14 08:07:59 +0100

    change imports to be more accurate about dependencies.

--- wasync/src/main/java/org/atmosphere/wasync/decoder/TrackMessageSizeDecoder.java
+++ wasync/src/main/java/org/atmosphere/wasync/decoder/TrackMessageSizeDecoder.java
@@ -20,7 +20,12 @@ import org.atmosphere.wasync.ReplayDecoder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Optional;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Function;
 import java.util.function.Predicate;
